43332d07bb1725022958e3dc7619cfd7e858db82
Naseem65 <57116813+Naseem65@users.noreply.github.com>
2020-05-27 22:48:26 -0400

    Modified and changed some files

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-
 function makeFetch(network) {
   var url = 'http://newsapi.org/v2/top-headlines?sources=' + network + '&apiKey=926ea2a6d4474d838091fd2e96217ef0';
-  return fetch(url)
+  var req = new Request(url);
+  return fetch(req)
     .then(function (response) {
       return response.json();
     })
     .then(function (json) {
       var articles = json.articles;
       console.log(articles);
+      window.open(articles[0].https://www.cnbc.com")
     })
 }
